eb0a20c6f938a3b1a468cb51ed711bf43d62cf7f
VeraLIMS <numbly-degree-disk@duck.com>
2025-06-05 08:02:20 +0200

    docs: update bug audit report

--- modules/management/directus_tools/__init__.py
+++ modules/management/directus_tools/__init__.py
@@ -1,1 +1,5 @@
+"""Expose Directus utility helpers."""
+
 from .directus_wizard import run_directus_wizard
+
+__all__ = ["run_directus_wizard"]
---
+++ tests/test_directus_tools_init.py
@@ -0,0 +1,9 @@
+import inspect
+
+from modules.management.directus_tools import run_directus_wizard
+from modules.management.directus_tools import directus_wizard as dw
+
+
+def test_run_directus_wizard_export():
+    assert run_directus_wizard is dw.run_directus_wizard
+    assert inspect.isfunction(run_directus_wizard)
